8e69f9b7c8d0f91d3cc7bdc68df9e2ed6050e593
Duvall Pinkney <14938639+Dpinkney001@users.noreply.github.com>
2021-11-14 04:23:58 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,152 +1,180 @@
-package com.example.tipcalculator4;
-
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.SeekBar;
-import android.widget.TextView;
+package com.example.finalproject1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.text.BreakIterator;
-import java.text.NumberFormat;
+import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-        implements TextWatcher, SeekBar.OnSeekBarChangeListener {
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Objects;
+import java.util.Scanner;
 
-    private TextView textViewBillAmount;
+public class MainActivity extends AppCompatActivity implements StackInterface {
 
-    //declare the variables for the calculations
-    private double billAmount = 0.0;
-    private double percent = .15;
-
-    //set the number formats to be used for the $ amounts , and % amounts
-    private final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
-    private final NumberFormat percentFormat = NumberFormat.getPercentInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //add Listeners to Widgets
-        //declare your variables for the widgets
-        EditText editTextBillAmount = (EditText) findViewById(R.id.editText_BillAmount);
- /*  Note: each View that will be retrieved using findViewById needs to map to a View with the matching id
-Example: editTextBillAmount
-Needs to map to a View with the following: android:id="@+id/editText_BillAmount
-*/
-        editTextBillAmount.addTextChangedListener((TextWatcher) this);
+        textView = findViewById(R.id.textView);
 
-        textViewBillAmount = (TextView) findViewById(R.id.textView_BillAmount);
+    }
+
+    public void createTextFile(View view){
+        Scanner inputStream = null;
+        PrintWriter outputStream = null;//
+
+        try {
+            inputStream = new Scanner(new File(fileName + ".txt"));
+            inputStream.nextLine();
+            while (inputStream.hasNextLine()) {
+                String data = inputStream.nextLine();
+    }
+
+    /////////////////////////////////////////////////////////////////
+    //--------------------stack operations--------------------------
+    //  createStack()
+    // creates an empty stack
+    //----------------------------------------------------------
+    // isEmpty():boolean{query}
+    // checks if stack is empty
+    //-------------------------------------------------------------
+    public boolean isEmpty(){
+
+    };
+    //------------------------------------------------------------
+    //         push(in newItem:stackItemType) throws stackexception
+    //  adds new item to the top of the stack. throws exception if the
+    //  insertion is not successful
+    //----------------------------------------------------------------------------
+
+
+    //----------------------------------------------------------------------------
+    // pop():StackItemType throws StackException
+    //  retrieves and then removes the top of the stack( the item that was most receintly added)
+    //  throws the stackexceptions
+    // if the deletion is not successfull
+    //-----------------------------------------------------------------------------------------
+    @Override
+    public Object pop() throws StackException{}
+
+    @Override
+    public Object peek() throws StackException {
+        return null;
+    }
+
+    //-------------------------------------------------------------------------------------------
+    // popAll()
+    // removes all items from the stack
+    //-------------------------------------------------------------------------------------------
+    @Override
+    public void popAll(){
+
     }
 
     @Override
-    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    public void push(Object newItem) throws StackException {
 
     }
 
-    /*
-    Note:   int i, int i1, and int i2
-            represent start, before, count respectively
-            The charSequence is converted to a String and parsed to a double for you
-     */
-    @Override
-    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        Log.d("MainActivity", "inside onTextChanged method: charSequence= " + charSequence);
-        //surround risky calculations with try catch (what if billAmount is 0 ?
-        //charSequence is converted to a String and parsed to a double for you
-        billAmount = Double.parseDouble(charSequence.toString()) / 100;
-        Log.d("MainActivity", "Bill Amount = " + billAmount);
-        //setText on the textView
-        textViewBillAmount.setText(currencyFormat.format(billAmount));
-        //perform tip and total calculation and update UI by calling calculate
-        calculate();//uncomment this line
-    }
+    //---------------------------------------------------------------------------------------------
+    // peek():stackItemType {query} throws stackexception
+    //
+    public class tasksOrderStack implements  StackInterface{
+        final int MAX_STACK_SIZE = 25;
+        private Objects items[];
+        private string textFileName;
+        private int top;
+        private int priority;
 
-    @Override
-    public void afterTextChanged(Editable editable) {
+        enum priority = {high, medium, low };// 0, 1 , 2
 
-    }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-        percent = progress / 100; //calculate percent based on seeker value
-        calculate();
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-
-    }
-
-    // calculate and display tip and total amounts
-    private void calculate() {
-        Log.d("MainActivity", "inside calculate method");
-        //uncomment below
-
-        // format percent and display in percentTextView
-        BreakIterator textViewPercent = null;
-        assert false;
-        textViewPercent.setText(percentFormat.format(percent));
-
-        // calculate the tip and total
-        double tip = billAmount * percent;
-        //use the tip example to do the same for the Total
-
-        // display tip and total formatted as currency
-        //user currencyFormat instead of percentFormat to set the textViewTip
-        BreakIterator tipTextView = null;
-        if (true) {
-            throw new AssertionError();
+        public tasksOrderStack(){
+            items = (Objects[]) new Object[MAX_STACK_SIZE];
+            top = -1;
         }
-        tipTextView.setText(currencyFormat.format(tip));
-        //use the tip example to do the same for the Total
-        double totalAmount = 0;
-        totalAmount = tip + billAmount;
-        textViewBillAmount.setText((int) totalAmount);
-    }
 
-    public void noCalculate(View view){
-        // calculate the tip and total
-        double tip = billAmount * percent;
-        //use the tip example to do the same for the Total
-
-        // display tip and total formatted as currency
-        //user currencyFormat instead of percentFormat to set the textViewTip
-        BreakIterator tipTextView = null;
-        if (true) {
-            throw new AssertionError();
+        public void task(){
+        textFileName = null;
+        priority = null;
         }
-        tipTextView.setText(currencyFormat.format(tip));
-        //use the tip example to do the same for the Total
-        double totalAmount = 0;
-        totalAmount = tip + billAmount;
-        textViewBillAmount.setText((int) totalAmount);
+
+        public void setTasks(Object task()){
+            textFileName = setText(textFileName)
+        }
     }
 
-    public void tipCalculate(View view) {
-        // rounds up tip before the total is calculated
-        double newTotal = 0;
-        double tip = 0;
-        tip = billAmount * percent;
-        newTotal = Math.round(tip) + billAmount;
-        textViewBillAmount.setText((int) newTotal);
+
+        @Override
+        public boolean isEmpty() {
+            return top < 0;
+        }
+
+        @Override
+        public void popAll() {
+            items = (Objects[]) new Object[MAX_STACK_SIZE];
+            top = -1;
+
+        }
+
+        @Override
+        public void push(Object newItem) throws StackException {
+            if(!isFull()){
+                items[++top] = (Objects) newItem;
+            }else{
+                throw new StackException("StackException on push: stack full");
+            }// end else
+
+        }// end push
+
+        @Override
+        public Object pop() throws StackException {
+            if(!isEmpty()){
+                return items[top--];
+            }else{
+                throw new StackException("StackException on pop: stack empty");
+            }// end else
+        }// end pop
+
+        @Override
+        public Object peek() throws StackException {
+            if (!isEmpty()){
+                return items[top];
+            }else{
+                throw new StackException(" STack Exception on peek: - stack empty");
+            }// end else
+        }// end peek
+
+        @Override
+        public boolean isFull() {
+            return top == MAX_STACK_SIZE-1;
+        }
+    }
+}
+
+    public void writeToFile(View view) {
+
     }
 
-    public void totalCalculate(View view) {
-        // tip and bill is exact. total is rounded up.
-        double tip;
-        tip = billAmount * percent;
-        double newTotal = tip + billAmount;
-        double newTotalRounded = Math.round(newTotal);
-        textViewBillAmount.setText((int) newTotalRounded);
+    public void saveToFile(View view) {
+    }
+
+    public void lowPriorityTask(View view) {
+
+
+    }
+
+    public void mediumPriorityTask(View view) {
+    }
+
+    public void highPriorityTask(View view) {
+    }
+
+        public setPriorityLevel(enum priority){
+
+        }
+
+    public void saveToFile(View view) {
     }
 }
